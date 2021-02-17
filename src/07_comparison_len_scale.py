3e384edb0cad50523ecab1efd624f161ec077e06
MuellerSeb <mueller.seb@posteo.de>
2021-02-17 16:22:16 +0100

    Update len_scale comparison plot to single col

--- src/07_comparison_len_scale.py
+++ src/07_comparison_len_scale.py
@@ -8,7 +8,8 @@ from anaflow import theis, ext_theis_2d
 
 # plotting style
 plt.style.use("ggplot")
-plt.rcParams.update({"font.size": 16})
+# increase fontsize of plots, prevent type 3 fonts in pdf output
+plt.rcParams.update({"font.size": 16, "pdf.fonttype": 42, "ps.fonttype": 42})
 
 # file extension of the saved plots
 file_ext = ".pdf"
@@ -28,7 +29,7 @@ time_labels = ["10 s", "10 min", "10 h"]
 time = [10, 600, 36000]  # 10s, 10min, 10h
 rad = np.geomspace(0.02, 4)  # radius from the pumping well in [0, 4]
 var = 0.5  # variance of log-transmissivity
-len_scales = [2, 5, 10, 20, 40]  # correlation length of log-transmissivity
+len_scales = [5, 10, 20, 40]  # correlation length of log-transmissivity
 TG = 1e-4  # the geometric mean transmissivity
 TH = TG * np.exp(-var / 2.0)  # the harmonic mean transmissivity
 S = 1e-4  # storativity
@@ -36,7 +37,7 @@ rate = -1e-4  # pumping rate
 
 tg_str = "Theis($T_G$)"
 th_str = "Theis($T_H$)"
-ex_str = r"extended Theis: $\ell=$"
+ex_str = "$h^{{\\mathrm{{eff}}}}(\\ell={})$"
 
 head_TG = theis(time, rad, S, TG, rate)
 head_TH = theis(time, rad, S, TH, rate)
@@ -45,14 +46,14 @@ for i, len_scale in enumerate(len_scales):
     head_ef.append(ext_theis_2d(time, rad, S, TG, var, len_scale, rate))
 time_ticks = []
 
-fig, ax = plt.subplots(figsize=[11, 5])
+fig, ax = plt.subplots(figsize=[7.5, 6])
 
 for i, step in enumerate(time):
     col = "C" + str(1)
     label_TG = tg_str if i == 0 else None
     label_TH = th_str if i == 0 else None
     for j, len_scale in enumerate(len_scales):
-        label_ef = ex_str + str(len_scale) if i == 0 else None
+        label_ef = ex_str.format(len_scale) if i == 0 else None
         ax.plot(
             rad,
             head_ef[j][i],
@@ -76,11 +77,12 @@ for i, step in enumerate(time):
     )
 ax.set_xlabel("$r$ in [m]")
 ax.set_ylabel("$h$ in [m]")
-ax.legend(loc="center left", bbox_to_anchor=(1.2, 0.5))
 ax.set_xlim([0, rad[-1]])
 ax.set_ylim([-2, 0])
-ax.locator_params(axis='y', nbins=5)
+ax.locator_params(axis="y", nbins=5)
+ax.legend(loc="lower center", ncol=3, bbox_to_anchor=(0.5, -0.45))
 fig.tight_layout()
+fig.subplots_adjust(bottom=0.3)
 fig.savefig(
     os.path.join(results, "len_scale_comparison" + file_ext),
     bbox_inches="tight",
